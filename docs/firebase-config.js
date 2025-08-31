76efd8acbc2cfba9e76ab1d643fa77dc4b20485c
Mallak Elsayed <malaaakmohamedd99@gmail.com>
2025-08-31 14:47:57 +0300

    Add web build to docs for GitHub Pages

---
+++ docs/firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOPdZecpS45-kaG5UgTK5WSraxVNEOpH0",
+  authDomain: "doctorcrudweb.firebaseapp.com",
+  projectId: "doctorcrudweb",
+  storageBucket: "doctorcrudweb.firebasestorage.app",
+  messagingSenderId: "579739380756",
+  appId: "1:579739380756:web:c0de6b8270554421254b01",
+  measurementId: "G-GN229T7NJ9"
+};
+
+// Export config
+export default firebaseConfig;
